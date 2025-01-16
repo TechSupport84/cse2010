563d8909a6e486299ba463af2d939162e1a48f14
TechSupport84 <trainingtech84@gmail.com>
2025-01-16 12:50:15 +0300

    added

--- week02/Resumes/Job.cs
+++ week02/Resumes/Job.cs
@@ -1,5 +1,4 @@
 using System;
-
 public class Job
 {
     public string _jobTitle;
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -9,18 +9,28 @@ class Program
         job1._company = "Microsoft";
         job1._startYear = 2019;
         job1._endYear = 2022;
-
+         
         Job job2 = new Job();
         job2._jobTitle = "Manager";
         job2._company = "Apple";
-        job2._startYear = 2022;
-        job2._endYear = 2023;
+        job2._startYear = 2024;
+        job2._endYear = 2025;
+
+        Job job3  = new Job();
+        job3._jobTitle = "English Teacher";
+        job3._company = "LapSchool";
+        job3._startYear = 2021;
+        job3._endYear = 2025;
 
         Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
+        myResume._name = "James LookMan";
 
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
+
+        myResume._name = "Jeancy Mpoyi";
+        myResume._jobs.Add(job3);
+    
 
         myResume.Display();
     }
