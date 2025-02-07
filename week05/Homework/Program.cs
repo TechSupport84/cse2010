df3a70392bf5ed4cce3ea58ca165f5b06d793f48
TechSupport84 <trainingtech84@gmail.com>
2025-02-07 17:49:43 +0300

    added

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,16 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Create a base "Assignment" object
         Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
         Console.WriteLine(a1.GetSummary());
 
-        // Now create the derived class assignments
         MathAssignment a2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        MathAssignment a4 = new MathAssignment("Jeancy Mpoyi ", "Fractions", "7.5", "9-9");
+        Console.WriteLine(a4.GetSummary());
+        Console.WriteLine(a4.GetHomeworkList());
         Console.WriteLine(a2.GetSummary());
         Console.WriteLine(a2.GetHomeworkList());
 
-        WritingAssignment a3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        WritingAssignment a3 = new WritingAssignment("John", "European History", "The Causes of World War II");
         Console.WriteLine(a3.GetSummary());
         Console.WriteLine(a3.GetWritingInformation());
     }
